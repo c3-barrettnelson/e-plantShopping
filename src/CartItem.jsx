32e0de37f35badf7bbc9f5907b43836dc01ad283
c3-barrettnelson <barrett.nelson@c3.ai>
2025-02-01 17:00:53 -0800

    add css

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,7 +21,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleCheckoutShopping = (e) => {
-    alert('Functionality to be added for future reference');
+    alert('Coming soon!');
   };
 
 
@@ -78,7 +78,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="checkout-button" onClick={() => handleCheckoutShopping()}>Checkout</button>
       </div>
     </div>
   );
